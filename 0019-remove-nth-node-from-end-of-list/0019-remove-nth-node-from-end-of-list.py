fb7c2df1e2848e615bd45f771453266d31e8120b
NELLIKUDURU TARUN <148840821+Tarun7386@users.noreply.github.com>
2025-07-19 23:10:40 +0530

    Time: 0 ms (100%), Space: 12.4 MB (88.38%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -3,6 +3,30 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+        # cnt=0
+        # temp=head
+        # p=head  
+        # while(temp!=None):
+        #     cnt+=1
+        #     temp=temp.next
+        # K=cnt-n
+        # if K==0:
+        #     return head.next
+        # l=0
+        # while(p!=None):
+        #     l+=1
+        #     d=p
+        #     p=p.next
+        #     if l==K:
+        #         d.next=d.next.next
+        # if head==None:
+        #     return None
+        # return head
+
+
+
+
+
 class Solution(object):
     def removeNthFromEnd(self, head, n):
         """
@@ -10,26 +34,26 @@ class Solution(object):
         :type n: int
         :rtype: Optional[ListNode]
         """
-        cnt=0
-        temp=head
-        p=head  
-        while(temp!=None):
-            cnt+=1
-            temp=temp.next
-        K=cnt-n
-        if K==0:
+        fast=head
+        for i in range(n):
+            fast=fast.next
+        if fast is None:
             return head.next
-        l=0
-        while(p!=None):
-            l+=1
-            d=p
-            p=p.next
-            if l==K:
-                d.next=d.next.next
-        if head==None:
-            return None
+        slow=head
+        while(fast.next!=None):
+            slow=slow.next
+            fast=fast.next
+        slow.next=slow.next.next
         return head
 
+        
+        
+
+
+
+
+
+          
 
 
 
