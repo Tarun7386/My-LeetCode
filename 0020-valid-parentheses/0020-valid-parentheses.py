cca19756b1765c5e13a6dad79c0949479b14fde2
NELLIKUDURU TARUN <148840821+Tarun7386@users.noreply.github.com>
2025-09-23 15:55:58 +0530

    Time: 8 ms (7.36%), Space: 12.5 MB (88.15%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+        
+        for char in s:
+            if char in mapping.values():
+                stack.append(char)
+            elif char in mapping:
+                if not stack or stack.pop() != mapping[char]:
+                    return False
+            else:
+               
+                return False
+        
+        return not stack
