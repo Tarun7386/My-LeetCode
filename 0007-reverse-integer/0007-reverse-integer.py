e84c003a1b42fcc2576f6a844a222a829aa2b5ef
NELLIKUDURU TARUN <148840821+Tarun7386@users.noreply.github.com>
2025-09-27 01:47:35 +0530

    Time: 18 ms (55.4%), Space: 12.5 MB (50.03%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -4,12 +4,25 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        n=abs(x)
-        rev=0
-        while(n>0):
-            digit=n%10
-            rev=rev*10+digit
-            n=n//10
-        if rev < -2**31 or rev > 2**31 - 1:
+        sign = -1 if x < 0 else 1
+        n = abs(x)
+        rev = 0
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31
+
+        while n != 0:
+            digit = n % 10
+            n //= 10
+
+            
+            if rev > (INT_MAX - digit) // 10:
+                return 0
+
+            rev = rev * 10 + digit
+
+        rev *= sign
+
+        if rev < INT_MIN or rev > INT_MAX:
             return 0
-        return rev if x>0 else -rev
+
+        return rev
